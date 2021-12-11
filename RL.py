b97ca4770cee56b8d77a98596ef2a6b26e966d03
TIMcre <92308299+TIMcre@users.noreply.github.com>
2021-12-11 16:03:17 +0100

    Delete RL.py
    deleting old stuff

--- RL.py
+++
@@ -1,8 +0,0 @@
-
-ti = int(input("How many players "))
-ch = ["Bloodhound", "Gibraltar", "Lifeline", "Pathfinder", "Wraith", "Bangalore", "Caustic", "Mirage",
-    "Octane", "Wattson", "Crypto", "Revenant", "Loba", "Rampart", "Horizon", "Fuse", "Valkyrie", "Seer", "Ash"]
-chs = random.sample(ch, ti)
-
-for i in chs:   
-    print(i)   
